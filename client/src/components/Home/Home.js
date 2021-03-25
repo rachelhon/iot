a440ac0f8ac8980642fc0975c3f13f5dcb086f77
rachelhon <khon@wisc.edu>
2021-03-25 03:04:18 -0500

    log in front end

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getDevices } from '../../actions/devices';
+
+import Devices from '../Devices/Devices';
+import Form from '../Form/Form';
+
+const Home = () => {
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(getDevices());
+    }, [dispatch]);
+  
+    return (
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+             <Devices/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    );
+  };
+  
+export default Home;
