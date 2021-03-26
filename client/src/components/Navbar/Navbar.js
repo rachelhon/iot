92b9027a30e0695bc253521d23870df8e16022c3
Austin-Torres <aus10hawk@yahoo.com>
2021-03-26 17:40:58 -0500

    update navbar to include welcome

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
             {user ? (
               <div className={classes.profile}>
                 <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                <Typography className={classes.userName} variant="h6">Welcome, {user.result.name}</Typography>
                 <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
               </div>
             ) : (
