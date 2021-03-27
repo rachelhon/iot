f91b0d07f64c2932b5df398cd906a0ee0c85ab58
Austin-Torres <aus10hawk@yahoo.com>
2021-03-26 19:54:43 -0500

    updated navbar avatar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -45,8 +45,9 @@ const Navbar = () => {
         {/* Display user's information based on their profile/ Google account */}
         {user ? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+            
             <Typography className={classes.userName} variant="h6">Welcome, {user.result.name}</Typography>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
