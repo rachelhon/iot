8740db75c6f4a28df7239e6bee60c531e84d99a2
Austin-Torres <aus10hawk@yahoo.com>
2021-04-06 19:25:29 -0500

    changed device layout

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,40 +1,41 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Container, Grow, Grid, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { getDevices } from '../../actions/devices';
 
 import Devices from '../Devices/Devices';
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-  
-    useEffect(() => {
-      dispatch(getDevices());
-    }, [dispatch]);
-  
-    const handleAddDevice = (e) => {
-      e.preventDefault();
-      history.push('/AddDevice');
-    }
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    return (
-      <Grow in>
-        <Container>
-          <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
-            <Grid item xs={12} sm={7}>
-             <Devices/>
-            </Grid>
-            
-            <Button variant="contained" color="primary" onClick={handleAddDevice}>
-             + Add Device
+  useEffect(() => {
+    dispatch(getDevices());
+  }, [dispatch]);
+
+  const handleAddDevice = (e) => {
+    e.preventDefault();
+    history.push('/AddDevice');
+  }
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+          <Button variant="contained" color="primary" onClick={handleAddDevice}>
+            + Add Device
             </Button>
+          <Grid item xs={12} sm={7}>
+            <Devices />
           </Grid>
-          
-        </Container>
-      </Grow>
-    );
-  };
-  
+
+
+        </Grid>
+
+      </Container>
+    </Grow>
+  );
+};
+
 export default Home;
