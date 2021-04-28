a6bc0f2cff87fb6dc378039c63999537004a03e5
yashishvin <56137245+yashishvin@users.noreply.github.com>
2021-04-27 19:45:31 -0500

    Update ViewDevice.js

--- client/src/components/ViewDevice/ViewDevice.js
+++ client/src/components/ViewDevice/ViewDevice.js
@@ -35,11 +35,33 @@ const ViewDevices = () => {
 
 
   const enableSIM = () => {
+    fetch("https://api.podiotsuite.com/v3/assets/F3Wtestsim0001/unsuspend", {
+      body: JSON.stringify({"accountId":"bbcca514-257d-4860-a1b1-0a94de203f49" }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Access-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1ZGE2MDI2NmEyYjEzYjAwMTFmOGM1ZDcifQ.LHisgkyOYCj6lMoriJ5y6lN_G63ZrH0QSSCbNUxWUtA"
+      },
+      method: "PUT"
+    })
+
+   
+
 
   }
 
 
   const disableSIM = () => {
+    fetch("https://api.podiotsuite.com/v3/assets/F3Wtestsim0001/suspend", {
+      body: JSON.stringify({"accountId":"bbcca514-257d-4860-a1b1-0a94de203f49" }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Access-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1ZGE2MDI2NmEyYjEzYjAwMTFmOGM1ZDcifQ.LHisgkyOYCj6lMoriJ5y6lN_G63ZrH0QSSCbNUxWUtA"
+      },
+      method: "PUT"
+    })
+
 
   }
 
