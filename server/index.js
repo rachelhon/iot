7a79069b4fd3c6e355b53004f6f778a11306ef40
rachelhon <khon@wisc.edu>
2021-04-28 22:01:37 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ import cors from 'cors';
 import fetch from 'node-fetch';
 import deviceRoutes from './routes/devices.js';
 import userRouter from './routes/users.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -13,6 +15,10 @@ app.use(cors());
 
 app.use('/devices', deviceRoutes);
 app.use('/user', userRouter);
+
+app.get('/', (req,res) => {
+    res.send('Hello to Internet of Things Tracking App');
+});
 
 // const PodGroupAPI = "https://api.podiotsuite.com/v3/auth/token";
 // const PodGroupUsername = "lab@f3wireless.com";
